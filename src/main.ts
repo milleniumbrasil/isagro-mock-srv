690c53d16251b485708cfe36863709a3c21368e0
Malnati <ricardomalnati@gmail.com>
2024-09-11 17:42:29 -0300

    chore: Update npm dependencies and add @nestjs/swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,34 @@
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Habilita CORS para todas as origens
-  app.enableCors();
+  
+	app.enableCors({
+		allowedHeaders: 'Authorization, X-Requested-With, Content-Type, Accept',
+		origin: '*',
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+		credentials: true,
+	});
+
+const microserviceName = 'IsAgro API'; 
+
+const config = new DocumentBuilder()
+			.setTitle(microserviceName)
+			.setDescription(`${microserviceName} - documentação`)
+			.setVersion('1.0')
+			.addTag('system')
+			.addBearerAuth(
+				{ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+				'access_token'
+			)
+			.build();
+		const document = SwaggerModule.createDocument(app, config);
+
+		SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
