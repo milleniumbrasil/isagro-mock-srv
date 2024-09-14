b55d698eda4a59156eea0db034143a68da5ae7d6
Malnati <ricardomalnati@gmail.com>
2024-09-14 14:27:10 -0300

    Refactor OrganicasService to enhance data processing methods and add logging functionality

--- src/organicas/organicas.service.ts
+++ src/organicas/organicas.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from "@nestjs/common"
+
 import { IStackedData, IPercentualData } from "../types"
 import { BaseService } from "src/BaseService"
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class OrganicasService extends BaseService {
-	
+
+    private readonly logger = new Logger(OrganicasService.name);
+
 	public getStackedDataValues(): IStackedData[] {
 		return [
 			{ period: "1990", entry: ["grão", 100] },
@@ -54,7 +57,95 @@ export class OrganicasService extends BaseService {
 		]
 	}
 
+	protected reducePercentualByLabel(data: IStackedData[], label: string): IStackedData[] {
+		// Filtra os dados pelo label fornecido
+		const filteredByLabel: IStackedData[] = data.filter((stackedItem) => {
+			return stackedItem.entry[0] === label;
+		});
+		this.logger.log(`filteredByLabel: ${JSON.stringify(filteredByLabel, null, 2)}`);
+		// Identifica os períodos únicos
+		const uniquePeriods = data.reduce((acc, current) => {
+			acc.add(current.period);
+			return acc;
+		}, new Set());
+		this.logger.log(`uniquePeriods: ${JSON.stringify(uniquePeriods, null, 2)}`);
+		// Acumula os valores por período
+		const amountsByPeriod: IStackedData[] = [];
+		uniquePeriods.forEach((period) => {
+			filteredByLabel.forEach((stackedItem) => {
+				if (stackedItem.period === period) {
+					const latest = amountsByPeriod.find((item) => item.period === period);
+					if (latest) {
+						latest.entry[1] += stackedItem.entry[1];
+					} else {
+						amountsByPeriod.push({
+							period: stackedItem.period,
+							entry: [...stackedItem.entry] // Faz uma cópia do array de entry
+						});
+					}
+				}
+			});
+		});
+
+		this.logger.log(`accumulatedByLabel: ${JSON.stringify(amountsByPeriod, null, 2)}`);
+		// Calcula o total geral de todos os períodos
+		const totalSum = amountsByPeriod.reduce((acc, stackedItem) => acc + stackedItem.entry[1], 0);
+		// Calcula o percentual de cada período em relação ao total geral
+		const percentualByLabel: IStackedData[] = amountsByPeriod.map((stackedItem) => {
+			let percentual = (stackedItem.entry[1] / totalSum) * 100; // Percentual correto
+			if (percentual > 1) {
+				percentual = Math.round(percentual); // Arredonda para facilitar
+			} else {
+				percentual = parseFloat(percentual.toFixed(2)); // Ajusta para duas casas decimais
+			}
+			return {
+				period: stackedItem.period,
+				entry: [label, percentual]
+			};
+		});
+		this.logger.log(`percentualByLabel: ${JSON.stringify(percentualByLabel, null, 2)}`);
+		return percentualByLabel;
+	}
+
+	protected accumulatedByLabel(data: IStackedData[], label: string): IStackedData[] {
+		const filteredByLabel: IStackedData[] = data.filter((stackedItem) => {
+			return stackedItem.entry[0] === label;
+		});
+		const uniquePeriods = data.reduce((acc, current) => {
+			acc.add(current.period);
+			return acc;
+		}, new Set());
+		const accumulatedByLabel: IStackedData[] = [];
+		uniquePeriods.forEach((period) => {
+			filteredByLabel.forEach((stackedItem) => {
+				if (stackedItem.period === period) {
+					const latest = accumulatedByLabel.find((item) => item.period === period);
+					if (latest) {
+						latest.entry[1] += stackedItem.entry[1];
+					} else {
+						accumulatedByLabel.push({
+							period: stackedItem.period,
+							entry: [...stackedItem.entry] // Faz uma cópia do array de entry
+						});
+					}
+				}
+			});
+		});
+		return accumulatedByLabel;
+	}
+
+	protected reducePercentualByPeriod(data: IStackedData[]): IPercentualData[] {
+		const percentualsByPeriod = data.reduce((acc, stackedItem) => {
+			const period = stackedItem.period
+			const value = stackedItem.entry.reduce((acc, curr) => acc + curr[1], 0)
+			acc.push({ period, value });
+			return acc;
+		}, [] as IPercentualData[]);
+		return percentualsByPeriod;
+	}
+
 	public getPercentualData(): IPercentualData[] {
+		this.reducePercentualByLabel(this.getStackedDataValues(), "pastagem");
 		const result = [
 			{ period: "1990", value: 100 },
 			{ period: "1990", value: 150 },
