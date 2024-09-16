dd8688dd07eeb367b70903b86bc4de396410adad
Malnati <ricardomalnati@gmail.com>
2024-09-16 19:36:40 -0300

    Add endpoint to retrieve cities by state abbreviation; refactor city data structure and logging

--- src/city/city.controller.ts
+++ src/city/city.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger, Param } from '@nestjs/common';
 import { CityService } from './city.service';
 import { BaseController } from '../BaseController';
 import { ApiOperation, ApiResponse, ApiParam } from '@nestjs/swagger';
 
 @Controller('city')
 export class CityController extends BaseController<CityService> {
+
+	private readonly logger = new Logger(CityController.name)
+
   constructor(service: CityService) {
     super(service);
   }
@@ -22,4 +25,28 @@ export class CityController extends BaseController<CityService> {
     return this.service.getData();
   }
 
+  @ApiOperation({ summary: "Obter todos os dados de cidades de um estado" })
+  @ApiParam({
+	  name: "stateAbbreviation",
+	  required: true,
+	  description:
+		  "O codigo ISO do estado para o qual os dados devem ser retornados. Opções: SP, RJ, MG, etc.",
+	  example: "SP",
+	  enum: [
+		'AC', 'MS', 'RS', 'CE', 'RO', 'SC', 'SE', 'AP', 'PB', 'AL', 'PE', 'PR', 'RJ', 'MT',
+		'DF', 'AM', 'BA', 'SP', 'ES', 'PI', 'PA', 'RR', 'MA', 'TO', 'GO', 'RN', 'MG'
+		],
+  })
+  @ApiResponse({
+	  status: 200,
+	  description: "Dados de cidades retornados com sucesso.",
+	  type: Object,
+	  isArray: true,
+  })
+  @ApiResponse({ status: 500, description: "Erro no servidor." })
+  @Get(":stateAbbreviation")
+  getCitiesByState(@Param('stateAbbreviation') stateAbbreviation: string) {
+	  this.logger.log(`Buscando cidades do estado ${stateAbbreviation}.`);
+	  return this.service.getCitiesByState(stateAbbreviation)
+  }
 }
--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,59 +1,88 @@
 import { Injectable, Logger } from "@nestjs/common"
 import { BaseService } from "../BaseService"
+import { ICity } from "src/types";
 
 @Injectable()
 export class CityService extends BaseService {
+
 	private readonly logger = new Logger(CityService.name)
 
 	public getValidLabels(): string[] {
 		return [
-			"RJ",
-			"RJ",
-			"RJ",
-			"SP",
-			"SP",
-			"SP",
-			"MG",
-			"MG",
-			"MG",
-			"BA",
-			"BA",
-			"BA",
-			"RS",
-			"RS",
-			"RS",
-			"AM",
-			"AM",
-			"AM",
-			"PE",
-			"PE",
-			"PE",
-		]
+			'AC', 'MS', 'RS', 'CE', 'RO', 'SC', 'SE',
+			'AP', 'PB', 'AL', 'PE', 'PR', 'RJ', 'MT',
+			'DF', 'AM', 'BA', 'SP', 'ES', 'PI', 'PA',
+			'RR', 'MA', 'TO', 'GO', 'RN', 'MG'
+		   ]
 	}
 
 	public getData<T>(): T[] {
 		return [
-			{ uf: "RJ", cidade: "Rio de Janeiro" },
-			{ uf: "RJ", cidade: "Niterói" },
-			{ uf: "RJ", cidade: "Angra dos Reis" },
-			{ uf: "SP", cidade: "São Paulo" },
-			{ uf: "SP", cidade: "Campinas" },
-			{ uf: "SP", cidade: "Santos" },
-			{ uf: "MG", cidade: "Belo Horizonte" },
-			{ uf: "MG", cidade: "Uberlândia" },
-			{ uf: "MG", cidade: "Juiz de Fora" },
-			{ uf: "BA", cidade: "Salvador" },
-			{ uf: "BA", cidade: "Feira de Santana" },
-			{ uf: "BA", cidade: "Ilhéus" },
-			{ uf: "RS", cidade: "Porto Alegre" },
-			{ uf: "RS", cidade: "Caxias do Sul" },
-			{ uf: "RS", cidade: "Pelotas" },
-			{ uf: "AM", cidade: "Manaus" },
-			{ uf: "AM", cidade: "Parintins" },
-			{ uf: "AM", cidade: "Itacoatiara" },
-			{ uf: "PE", cidade: "Recife" },
-			{ uf: "PE", cidade: "Olinda" },
-			{ uf: "PE", cidade: "Caruaru" },
+			{ iso: "BR-AC", abbreviation: "AC", city: "Rio Branco" },
+			{ iso: "BR-AL", abbreviation: "AL", city: "Arapiraca" },
+			{ iso: "BR-AL", abbreviation: "AL", city: "Maceió" },
+			{ iso: "BR-AM", abbreviation: "AM", city: "Manaus" },
+			{ iso: "BR-AM", abbreviation: "AM", city: "Parintins" },
+			{ iso: "BR-AP", abbreviation: "AP", city: "Macapá" },
+			{ iso: "BR-BA", abbreviation: "BA", city: "Feira de Santana" },
+			{ iso: "BR-BA", abbreviation: "BA", city: "Ilhéus" },
+			{ iso: "BR-BA", abbreviation: "BA", city: "Salvador" },
+			{ iso: "BR-CE", abbreviation: "CE", city: "Fortaleza" },
+			{ iso: "BR-CE", abbreviation: "CE", city: "Juazeiro do Norte" },
+			{ iso: "BR-CE", abbreviation: "CE", city: "Sobral" },
+			{ iso: "BR-DF", abbreviation: "DF", city: "Brasília" },
+			{ iso: "BR-ES", abbreviation: "ES", city: "Vila Velha" },
+			{ iso: "BR-ES", abbreviation: "ES", city: "Vitória" },
+			{ iso: "BR-GO", abbreviation: "GO", city: "Anápolis" },
+			{ iso: "BR-GO", abbreviation: "GO", city: "Goiânia" },
+			{ iso: "BR-MA", abbreviation: "MA", city: "Imperatriz" },
+			{ iso: "BR-MA", abbreviation: "MA", city: "São Luís" },
+			{ iso: "BR-MG", abbreviation: "MG", city: "Belo Horizonte" },
+			{ iso: "BR-MG", abbreviation: "MG", city: "Contagem" },
+			{ iso: "BR-MG", abbreviation: "MG", city: "Juiz de Fora" },
+			{ iso: "BR-MG", abbreviation: "MG", city: "Uberlândia" },
+			{ iso: "BR-MS", abbreviation: "MS", city: "Campo Grande" },
+			{ iso: "BR-MS", abbreviation: "MS", city: "Dourados" },
+			{ iso: "BR-MT", abbreviation: "MT", city: "Cuiabá" },
+			{ iso: "BR-MT", abbreviation: "MT", city: "Rondonópolis" },
+			{ iso: "BR-PA", abbreviation: "PA", city: "Belém" },
+			{ iso: "BR-PA", abbreviation: "PA", city: "Marabá" },
+			{ iso: "BR-PA", abbreviation: "PA", city: "Santarém" },
+			{ iso: "BR-PB", abbreviation: "PB", city: "Campina Grande" },
+			{ iso: "BR-PB", abbreviation: "PB", city: "João Pessoa" },
+			{ iso: "BR-PE", abbreviation: "PE", city: "Caruaru" },
+			{ iso: "BR-PE", abbreviation: "PE", city: "Olinda" },
+			{ iso: "BR-PE", abbreviation: "PE", city: "Recife" },
+			{ iso: "BR-PI", abbreviation: "PI", city: "Parnaíba" },
+			{ iso: "BR-PI", abbreviation: "PI", city: "Teresina" },
+			{ iso: "BR-PR", abbreviation: "PR", city: "Curitiba" },
+			{ iso: "BR-PR", abbreviation: "PR", city: "Londrina" },
+			{ iso: "BR-PR", abbreviation: "PR", city: "Maringá" },
+			{ iso: "BR-RJ", abbreviation: "RJ", city: "Angra dos Reis" },
+			{ iso: "BR-RJ", abbreviation: "RJ", city: "Niterói" },
+			{ iso: "BR-RJ", abbreviation: "RJ", city: "Rio de Janeiro" },
+			{ iso: "BR-RN", abbreviation: "RN", city: "Mossoró" },
+			{ iso: "BR-RN", abbreviation: "RN", city: "Natal" },
+			{ iso: "BR-RO", abbreviation: "RO", city: "Porto Velho" },
+			{ iso: "BR-RR", abbreviation: "RR", city: "Boa Vista" },
+			{ iso: "BR-RS", abbreviation: "RS", city: "Caxias do Sul" },
+			{ iso: "BR-RS", abbreviation: "RS", city: "Pelotas" },
+			{ iso: "BR-RS", abbreviation: "RS", city: "Porto Alegre" },
+			{ iso: "BR-SC", abbreviation: "SC", city: "Blumenau" },
+			{ iso: "BR-SC", abbreviation: "SC", city: "Florianópolis" },
+			{ iso: "BR-SC", abbreviation: "SC", city: "Joinville" },
+			{ iso: "BR-SE", abbreviation: "SE", city: "Aracaju" },
+			{ iso: "BR-SP", abbreviation: "SP", city: "Campinas" },
+			{ iso: "BR-SP", abbreviation: "SP", city: "Santos" },
+			{ iso: "BR-SP", abbreviation: "SP", city: "São Paulo" },
+			{ iso: "BR-TO", abbreviation: "TO", city: "Palmas" },
 		] as T[]
+	}
+
+	public getCitiesByState(stateAbbreviation: string): ICity[] {
+		const cities: ICity[] = this.getData();
+		this.logger.log(`Buscando cidades do estado ${stateAbbreviation} em ${cities.length} registros.`);
+		console.log(`Buscando cidades em ${JSON.stringify(cities, null, 2)}`);
+		return cities.filter((item) => item.abbreviation === stateAbbreviation);
 	}
 }
